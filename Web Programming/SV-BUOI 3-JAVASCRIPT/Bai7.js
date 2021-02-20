02cbf1da726b93d9a1d5d6ebe2fa0b1ce422d91b
= <=>
2021-02-20 16:10:32 +0700

    File HK2 Nam3

--- Web Programming/SV-BUOI 3-JAVASCRIPT/Bai7.js
+++ Web Programming/SV-BUOI 3-JAVASCRIPT/Bai7.js
@@ -1,28 +1,45 @@
-
-
-function namSinh(calNS) {
-	var NamSinh = document.getElementsByName('optNamSinh')[0].value;
-		var oNS = new Date();
-		var oGNS = oNS.getFullYear();
-		for(var i=1940;i<=oGNS;i++) {
-			document.write('<option value="'+i+'">'+i+'</option>');
-		}
-		calNS = oGNS - NamSinh;
-		return calNS;
+function sNS() {
+	var oNS = new Date();
+	var YH = oNS.getFullYear();
+	for(var i=1940;i<=YH;i++) {
+		document.write('<option value="'+i+'">'+i+'</option>');
+	}
 }
 
-
-
 function Notification() {
-	var HT = document.getElementsByName('txtHoTen')[0].value;
-	/*var GioiTinh = document.getElementsByName('radGioiTinh')[0].value;
-	var MucLuong = document.getElementsByName('txtMucLuong')[0].value;
-	var HinhThucToanTG = document.getElementBysName('chkHinhThucToanTG')[0].value;
-	var ThongTin = document.getElementsByName('txtThongTin')[0].value;*/
 
+	var HT = document.getElementsByName('txtHoTen')[0].value;
 	if(HT == "") {
 		alert("Hãy nhập đầy đủ họ và tên");
 		return false;
 	}
+
+	var NS = document.getElementsByName('optNamSinh')[0].value;
+	var oNS = new Date();
+	var YH = oNS.getFullYear();
+	if(YH - NS < 16){
+		alert("Chưa đủ 16 tuổi");
+		return false
+	}
+
+	var mucLuong = document.getElementsByName('txtMucLuong')[0].value;
+	if(mucLuong != Number(mucLuong)){
+		alert("Hãy nhập giá trị bằng số!");
+		return false
+	}
+
+	var toanTG = document.getElementById('chkHinhThucToanTG').checked;
+	var banTG = document.getElementById('chkHinhThucBanTG').checked;
+	if(toanTG == false && banTG == false) {
+		alert("Hãy chọn thời gian làm việc");
+		return false;
+	}
+
 	return true;
+	
 }
+
+function showInfo() {
+		var csm = window.open('');
+		csm.document.write("dsjhsjdhsjhdsjhd");
+	}
