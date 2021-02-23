7a6011e8ca9249ee968c411c09d886c8289de9b1
= <=>
2021-02-23 11:08:09 +0700

    File HK2

--- Web Programming/SV-BUOI 3-JAVASCRIPT/Bai7.js
+++ Web Programming/SV-BUOI 3-JAVASCRIPT/Bai7.js
@@ -9,37 +9,62 @@ function sNS() {
 function Notification() {
 
 	var HT = document.getElementsByName('txtHoTen')[0].value;
+
+	var NS = document.getElementsByName('optNamSinh')[0].value;
+	var oNS = new Date();
+	var YH = oNS.getFullYear();
+
+	var mucLuong = document.getElementsByName('txtMucLuong')[0].value;
+
+	var toanTG = document.getElementById('chkHinhThucToanTG').checked;
+	var banTG = document.getElementById('chkHinhThucBanTG').checked;
+
 	if(HT == "") {
 		alert("Hãy nhập đầy đủ họ và tên");
 		return false;
 	}
 
-	var NS = document.getElementsByName('optNamSinh')[0].value;
-	var oNS = new Date();
-	var YH = oNS.getFullYear();
 	if(YH - NS < 16){
 		alert("Chưa đủ 16 tuổi");
 		return false
 	}
 
-	var mucLuong = document.getElementsByName('txtMucLuong')[0].value;
 	if(mucLuong != Number(mucLuong)){
 		alert("Hãy nhập giá trị bằng số!");
 		return false
 	}
 
-	var toanTG = document.getElementById('chkHinhThucToanTG').checked;
-	var banTG = document.getElementById('chkHinhThucBanTG').checked;
 	if(toanTG == false && banTG == false) {
 		alert("Hãy chọn thời gian làm việc");
 		return false;
 	}
-
-	return true;
-	
-}
-
-function showInfo() {
-		var csm = window.open('');
-		csm.document.write("dsjhsjdhsjhdsjhd");
+	if(toanTG == true && banTG == true){
+		alert("Hãy chọn 1 trong 2 hình thức làm việc");
+		return false;
 	}
+	
+	showInfo();
+	return true;
+}
+function showInfo() {
+
+		var csm = window.open('');
+		csm.document.write('Cám ơn! Bên dưới là thông tin đăng kí của bạn:');
+		csm.document.write('<br/>Họ tên: '+frm.txtHoTen.value);
+		csm.document.write('<br/>Năm Sinh: '+frm.optNamSinh.value);
+		if(frm.radGioiTinh[0].checked == true){
+			csm.document.write('<br/>Giới Tính: '+frm.radGioiTinh[0].value);
+		}
+		else{
+			csm.document.write('<br/>Giới Tính: '+frm.radGioiTinh[1].value);
+		}
+		csm.document.write('<br/>Mức Lương: '+frm.txtMucLuong.value);
+		if(frm.chkHinhThucToanTG.checked == true){
+			csm.document.write('<br/>Hình thức làm việc:'+frm.chkHinhThucToanTG.value);
+		}
+		else{
+			csm.document.write('<br/>Hình thức làm việc:'+frm.chkHinhThucBanTG.value);
+		}
+		csm.document.write('<br/>Thông tin bổ sung:'+frm.txtThongTin.value);
+	}
+
