beaab21213e618e8ecfc5f8a0ebeefc09ae6cc2d
= <=>
2021-03-02 21:28:58 +0700

    File HK2

---
+++ LTQL/PersonalProject/SqlProject/QLTSTBKhachSan.sql
@@ -0,0 +1,23 @@
+create database TSTBKhachSan
+go
+
+use TSTBKhachSan
+go
+
+create table ThietBi(
+	MaTB nvarchar (10),
+	TenTB nvarchar (255),
+	DonVi nvarchar (30),
+	SoLuong int,
+	DanhMuc nvarchar (255),
+	GiaTri int,
+	TGBaoHanh date,
+	TinhTrangTB nvarchar (30),
+	TinhTrangCPTB nvarchar (30),
+	BoPhanCP nvarchar (255),
+	DiaDiemCP nvarchar (30),
+	NCC nvarchar (255),
+	NgayMuaTB date
+)
+go
+
