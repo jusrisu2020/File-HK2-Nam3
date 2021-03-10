c32be08c47df25f88562c31e6ccd1fe4c0813d99
= <=>
2021-03-10 21:13:11 +0700

    File

---
+++ LTQL/PersonalProject/Winform/TaoButtonTuDong/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace TaoButtonTuDong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTaoButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTaoButton
+            // 
+            this.btnTaoButton.Location = new System.Drawing.Point(131, 117);
+            this.btnTaoButton.Name = "btnTaoButton";
+            this.btnTaoButton.Size = new System.Drawing.Size(126, 28);
+            this.btnTaoButton.TabIndex = 0;
+            this.btnTaoButton.Text = "Tạo Button";
+            this.btnTaoButton.UseVisualStyleBackColor = true;
+            this.btnTaoButton.Click += new System.EventHandler(this.btnTaoButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(419, 161);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnTaoButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTaoButton;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ LTQL/PersonalProject/Winform/TaoButtonTuDong/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaoButtonTuDong
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnTaoButton_Click(object sender, EventArgs e)
+        {
+            int n=10;
+
+            Button bt = new Button();
+            for(int i = 0; i < n; i++)
+            {
+                bt.DialogResult.ToString();
+            }
+          
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Button bt = new Button();
+            
+        }
+    }
+}
