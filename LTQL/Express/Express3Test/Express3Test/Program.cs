330cda0c5b45cdff8ce635a8eec70290261deb1d
= <=>
2021-03-14 14:56:09 +0700

    File

---
+++ LTQL/Express/Express3Test/Express3Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Express3Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
