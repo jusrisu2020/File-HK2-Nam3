994697ff10cafa0328ac96b1b487a0c39b8630d4
= <=>
2021-03-19 12:41:46 +0700

    File

--- Web Programming/SV-BUOI 3-JAVASCRIPT/Bai12a.js
+++ Web Programming/SV-BUOI 3-JAVASCRIPT/Bai12a.js
@@ -6,23 +6,26 @@
 - Phải có ít nhất một ký tự sau dấu . cuối cùng
 - Không có khoảng trắng trong địa chỉ mail
 */
-
+		
 function Send(){
 
 
 	var txtE = document.getElementsByName('txtEmail')[0].value;
-	var acong = "@";
-	var cham = ".";
 	
-	for(var i=0; i<txtE.length;i++){
+	var Pos_acong = txtE.indexOf("@");
+	var Pos_cham = txtE.indexOf(".");
 	
-			console.log(i);
-			if(txt.length[0] == ""){
-				alert("dsds");
-			}
+	if(Pos_acong == -1 
+		||Pos_acong == 0 
+		||Pos_cham == -1
+		|| Pos_cham>txtE.length+1
+		){
 		
+		alert("lỗi");
 		
-		
+
 	}
+	
+
 	
 }
