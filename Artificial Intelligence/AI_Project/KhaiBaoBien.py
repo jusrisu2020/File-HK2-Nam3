bb21f0593abbbdbbcf4401a5df6377652f6598fd
phavtt20@gmail.com <phavtt20@gmail.com>
2021-04-16 21:19:24 +0700

    Artificial Intelligence

---
+++ Artificial Intelligence/AI_Project/KhaiBaoBien.py
@@ -0,0 +1,20 @@
+#Khai Báo Biến
+def FunTong2So():
+	x = 1
+	y = 2
+	z = x+y
+	print(z)
+
+def Fun_Complex():
+	x=1j
+	print(x)
+
+def  Fun_Range():
+	x = list	
+	print(x(range(10)))
+
+
+
+Fun_Range()
+FunTong2So()
+Fun_Complex()
