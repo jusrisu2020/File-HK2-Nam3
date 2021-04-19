dc96eb3465a5bf4154b23d25ce32d81f362eae5e
= <=>
2021-04-19 21:08:11 +0700

    Upload SQL SERVER

--- Management Programming/PersonalProject/SqlProject/TaoMaTuSinhQuery.sql
+++ Management Programming/PersonalProject/SqlProject/TaoMaTuSinhQuery.sql
@@ -3,9 +3,7 @@ GO
 USE TestMaTuSinh
 GO
 
-USE QLCF
-GO
-
+DROP TABLE Ban
 
 create table Ban
 	(
@@ -15,12 +13,18 @@ create table Ban
 	)
 GO
 
+create table Ban2
+	(
+		BanID2	smallint	not null	identity(1,1)
+		,MaBan2	char(7) CONSTRAINT FKTEST FOREIGN KEY(MaBan2) REFERENCES dbo.Ban(MaBan)
+		,MoTa2	nvarchar(50)
+	)
+GO
 create proc procThemBan
 		@MoTa nvarchar(50)
 	as
 	begin
 		Declare @MaBan char(7)
-		DECLARE @Test INT = 0
 		Set @MaBan=(select IDENT_CURRENT('Ban'))
 
 		if exists (select * from Ban where BanID=@MaBan) 
@@ -36,19 +40,18 @@ go
 	exec procThemBan N'Mô tả 5'
 GO
 SELECT * FROM dbo.Ban
-
+go
 create proc procThemBan2
-		@MoTa nvarchar(50)
+		@MoTa2 char(7)
 	as
 	begin
-		Declare @MaBan char(7)
-		DECLARE @Test INT = 0
-		Set @MaBan=(select IDENT_CURRENT('Ban'))
+		Declare @MaBan2 char(7)
+		Set @MaBan2=(select IDENT_CURRENT('Ban2'))
 
-		if exists (select * from Ban where BanID=@MaBan) 
-			SET @MaBan=@MaBan+1
-			SET @MaBan='BA'+REPLICATE('0',2-LEN(@MaBan))+@MaBan
-			INSERT into Ban values(@MaBan,@MoTa)
+		if exists (select * from Ban where BanID=@MaBan2) 
+			SET @MaBan2=@MaBan2+1
+			SET @MaBan2='BA2'+REPLICATE('0',2-LEN(@MaBan2))+@MaBan2
+			INSERT into Ban values(@MaBan2,@MoTa2)
 	end
 GO
 go
