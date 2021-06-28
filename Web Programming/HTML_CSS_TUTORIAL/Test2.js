54e7664bf8f283a7bb70335c5ff3c39ab50ca6c3
= <=>
2021-06-28 22:24:46 +0700

    Upload 22:24p 28/6/2021

--- Web Programming/HTML_CSS_TUTORIAL/Test2.js
+++ Web Programming/HTML_CSS_TUTORIAL/Test2.js
@@ -1,5 +1,1 @@
-var Time = new Date();
-var btnTime = document.getElementById("btnTime");
-btnTime.addEventListener("click", () =>{
-    document.getElementById("TimeInfo").innerHTML = Time;
-})
+
---
+++ Web Programming/HTML_CSS_TUTORIAL/Welcome.js
@@ -0,0 +1,4 @@
+
+function Show(){
+    alert(ms);
+}
